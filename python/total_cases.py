50cd7355b378ad40d685c29e9f60de59a6e029bb
Rambod Rahmani <rambodrahmani@autistici.org>
2021-02-12 00:28:27 +0100

    Improve data preprocessing. Clean up and refactor source code.

--- python/total_cases.py
+++ python/total_cases.py
@@ -15,20 +15,20 @@ __license__ = "GPLv3"
 
 def plot():
     # load raw data
-    historical_df = util.loadHistoricalData()
+    historicalDF = util.loadHistoricalData()
 
     # group raw data by country aggregating on total cases
-    historical_df = historical_df.groupby("location", as_index = False)["total_cases"].last()
-    historical_df.columns = ['location', 'total_cases']
+    historicalDF = historicalDF.groupby("location", as_index = False)["total_cases"].last()
+    historicalDF.columns = ['location', 'total_cases']
 
     # sort by total cases count
-    historical_df = historical_df.sort_values(by=['total_cases'], ascending = False)
+    historicalDF = historicalDF.sort_values(by=['total_cases'], ascending = False)
 
     # extract top 15 countries by total cases count
-    historical_df = historical_df[0:15]
+    historicalDF = historicalDF[0:15]
 
     # plot top 15 countries total cases histogram
-    hist = historical_df.plot.bar(x = 'location', y = 'total_cases', rot = 0)
+    hist = historicalDF.plot.bar(x = 'location', y = 'total_cases', rot = 0)
     hist.set(title = "Confirmed COVID-19 Cases (Top 15 Worldwide)")
     hist.legend(["Total Cases"]);
     plt.xlabel('Country', fontsize = 16)
@@ -40,20 +40,20 @@ def plot():
 
 def perMilionPlot():
     # load raw data
-    historical_df = util.loadHistoricalData()
+    historicalDF = util.loadHistoricalData()
 
     # group raw data by country aggregating on total cases
-    historical_df = historical_df.groupby("location", as_index = False)["total_cases_per_million"].last()
-    historical_df.columns = ['location', 'total_cases_per_million']
+    historicalDF = historicalDF.groupby("location", as_index = False)["total_cases_per_million"].last()
+    historicalDF.columns = ['location', 'total_cases_per_million']
 
     # sort by total cases count
-    historical_df = historical_df.sort_values(by=['total_cases_per_million'], ascending = False)
+    historicalDF = historicalDF.sort_values(by=['total_cases_per_million'], ascending = False)
 
     # extract top 15 countries by total cases count
-    historical_df = historical_df[0:15]
+    historicalDF = historicalDF[0:15]
 
     # plot top 15 countries total cases histogram
-    hist = historical_df.plot.bar(x = 'location', y = 'total_cases_per_million', rot = 0)
+    hist = historicalDF.plot.bar(x = 'location', y = 'total_cases_per_million', rot = 0)
     hist.set(title = "Confirmed COVID-19 Cases Per One Million Population (Top 15 Worldwide)")
     hist.legend(["Total Cases"]);
     plt.xlabel('Country', fontsize = 16)
--- python/util.py
+++ python/util.py
@@ -5,13 +5,16 @@
 ################################################################################
 
 import os.path
+import numpy as np
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 __author__ = "Rambod Rahmani"
 __copyright__ = "Copyright (C) 2021 Rambod Rahmani"
 __license__ = "GPLv3"
 
 HISTORICAL_DATASET_PATH = "historical-covid-data.csv"
+PREPROCESSED_HISTORICAL_DATASET_PATH = "resampled-historical-covid-data.csv"
 
 ##
 # Loads and preprocesses the historical dataframe.
@@ -19,21 +22,58 @@ HISTORICAL_DATASET_PATH = "historical-covid-data.csv"
 def loadHistoricalData():
     # check if the csv file exists
     if os.path.isfile(HISTORICAL_DATASET_PATH):
-        # read csv file
-        historical_df = pd.read_csv(HISTORICAL_DATASET_PATH)
+        if os.path.isfile(PREPROCESSED_HISTORICAL_DATASET_PATH):
+            historicalDF = pd.read_csv(PREPROCESSED_HISTORICAL_DATASET_PATH)
+            return historicalDF
+        else:
+            # read csv file
+            historicalDF = pd.read_csv(HISTORICAL_DATASET_PATH)
+            
+            # remove world and continents data
+            historicalDF = historicalDF[~historicalDF.location.str.contains("Asia")]
+            historicalDF = historicalDF[~historicalDF.location.str.contains("World")]
+            historicalDF = historicalDF[~historicalDF.location.str.contains("Africa")]
+            historicalDF = historicalDF[~historicalDF.location.str.contains("Europe")]
+            historicalDF = historicalDF[~historicalDF.location.str.contains("America")]
+            historicalDF = historicalDF[~historicalDF.location.str.contains("Australia")]
+            historicalDF = historicalDF[~historicalDF.location.str.contains("Antarctica")]
+            historicalDF = historicalDF[~historicalDF.location.str.contains("Europe Union")]
+            historicalDF = historicalDF[~historicalDF.location.str.contains("North America")]
+            historicalDF = historicalDF[~historicalDF.location.str.contains("South America")]
 
-        # remove world and continents data
-        historical_df = historical_df[~historical_df.location.str.contains("Asia")]
-        historical_df = historical_df[~historical_df.location.str.contains("World")]
-        historical_df = historical_df[~historical_df.location.str.contains("Africa")]
-        historical_df = historical_df[~historical_df.location.str.contains("Europe")]
-        historical_df = historical_df[~historical_df.location.str.contains("America")]
-        historical_df = historical_df[~historical_df.location.str.contains("Australia")]
-        historical_df = historical_df[~historical_df.location.str.contains("Antarctica")]
-        historical_df = historical_df[~historical_df.location.str.contains("Europe Union")]
-        historical_df = historical_df[~historical_df.location.str.contains("North America")]
-        historical_df = historical_df[~historical_df.location.str.contains("South America")]
+            # only select columns of interest for the data mining process
+            historicalDF = historicalDF[['iso_code', 'continent', 'location', 'date', 'total_cases', 'new_cases', 'total_deaths', 'new_deaths', 'total_cases_per_million', 'new_cases_per_million', 'total_deaths_per_million', 'new_deaths_per_million']]
 
-        return historical_df
+            # preprocessing: replace negative values with NaN
+            historicalDF[historicalDF <= 0] = np.nan
+
+            # extract location column values
+            byLocation = historicalDF.groupby("location", as_index = False)["total_cases"].last()
+            byLocation.columns = ['location', 'total_cases']
+            locations = byLocation['location'].values
+
+            # empty dataframe with the same columns
+            imputedDF = pd.DataFrame(columns = historicalDF.columns)
+
+            # preprocessing: replace missing values with the mean by location
+            for location in locations:
+                meanImputer = SimpleImputer(missing_values = np.nan, strategy = 'constant', fill_value = 0)
+                tempDF = historicalDF[historicalDF['location'] == location]
+                totalCases = pd.DataFrame(meanImputer.fit_transform(tempDF[['total_cases']]))
+                newCases = pd.DataFrame(meanImputer.fit_transform(tempDF[['new_cases']]))
+                totalDeaths = pd.DataFrame(meanImputer.fit_transform(tempDF[['total_deaths']]))
+                newDeaths = pd.DataFrame(meanImputer.fit_transform(tempDF[['new_deaths']]))
+                totalCasesPerMillion = pd.DataFrame(meanImputer.fit_transform(tempDF[['total_cases_per_million']]))
+                newCasesPerMillion = pd.DataFrame(meanImputer.fit_transform(tempDF[['new_cases_per_million']]))
+                totalDeathsPerMillion = pd.DataFrame(meanImputer.fit_transform(tempDF[['total_deaths_per_million']]))
+                newDeathsPerMillion = pd.DataFrame(meanImputer.fit_transform(tempDF[['new_deaths_per_million']]))
+
+                # check all values are available for this location
+                if not totalCases.empty:
+                    composedDF = pd.DataFrame(list(zip(tempDF['iso_code'].values, tempDF['continent'].values, tempDF['location'].values, tempDF['date'].values, totalCases[0].values, newCases[0].values, totalDeaths[0].values, newDeaths[0].values, totalCasesPerMillion[0].values, newCasesPerMillion[0].values, totalDeathsPerMillion[0].values, newDeathsPerMillion[0].values)), columns = historicalDF.columns)
+                    imputedDF = imputedDF.append(composedDF, ignore_index = True, sort = True)
+            
+            imputedDF.to_csv(PREPROCESSED_HISTORICAL_DATASET_PATH, index=False)
+            return imputedDF
     else:
         print("Historical data .CSV file not found.")
