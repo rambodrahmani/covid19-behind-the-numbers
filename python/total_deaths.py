4d0dd14be6bbd01b8eb1ed52ca6b227e51f27985
Rambod Rahmani <rambodrahmani@autistici.org>
2021-02-12 23:25:59 +0100

    Improve time series clustering implementation.

--- python/total_deaths.py
+++ python/total_deaths.py
@@ -84,54 +84,42 @@ def clusteringPlot():
         historicalDF = historicalDF[['date', 'location', 'new_deaths_per_million']]
 
         # convert dataframe to multiple time series structure
-        historicalDF = historicalDF.pivot(index='date', columns='location', values='new_deaths_per_million')
+        historicalDF = historicalDF.pivot(index='location', columns='date', values='new_deaths_per_million')
 
         # replace NaNs generated in the conversion to time series with zeros
         historicalDF = historicalDF.fillna(0)
-
-        # convert dataframe Index to DatetimeIndex
-        historicalDF.index = pd.to_datetime(historicalDF.index)
-
-        # resample data weekly to obtain a better plot
-        weeklyDF = historicalDF.resample('W').sum()
         
-        # K-means
-        cluster = 3
-        X_train = historicalDF.rolling(7, axis=1, min_periods=1).mean().fillna(0)
-        colors = ['blue', 'red', 'green']
-        names = ['blue cluster','red cluster','green cluster']
+        # K-means time series clustering parameters
+        nClusters = 3
+        clustersColors = ['blue', 'red', 'green']
+        clustersNames = ['Blue Cluster', 'Red Cluster', 'Green Cluster']
+
+        # random seed used to initialize the centers
         seed = 1
         np.random.seed(seed)
-        X_train = to_time_series_dataset(X_train.copy())
 
-        print('COVID-19 deaths vs time curves')
-        km = TimeSeriesKMeans(n_clusters=cluster, verbose=True, random_state=seed,
-                                max_iter=10)
+        # extract training set and convert to time series dataset
+        historicalDFTrain = historicalDF.rolling(7, axis = 1, min_periods = 1).mean().fillna(0)
+        historicalDFTrain = to_time_series_dataset(historicalDFTrain.copy())
 
-        y_pred = km.fit_predict(X_train)
-        clusters = pd.Series(data=y_pred, index=historicalDF.index)
+        # K-means time series clustering Model using Euclidean distance
+        euclideanKM = TimeSeriesKMeans(n_clusters = nClusters, metric = "euclidean", verbose = False, random_state = seed, max_iter = 50)
+        y_pred = euclideanKM.fit_predict(historicalDFTrain)
+        clusters = pd.Series(data = y_pred, index = historicalDF.index)
 
-        f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True,figsize=(5, 8))
-
-        for yi,cl,xs in zip(range(cluster),[2,1,0],[ax1,ax2,ax3]):
-            data = historicalDF.rolling(7, axis=1, min_periods=1).mean().fillna(0).loc[clusters[clusters == cl].index]
-            data.T.plot(legend=False, alpha=.2,color='black', ax=xs)
-            data.mean(axis=0).plot(linewidth=3., color=colors[cl], ax=xs)
+        # plot generated clusters
+        f, (ax1, ax2, ax3) = plt.subplots(3, sharex = True, sharey = True, figsize = (5, 8))
+        for yi, cl, xs in zip(range(nClusters), [2,1,0], [ax1, ax2, ax3]):
+            data = historicalDF.rolling(7, axis = 1, min_periods = 1).mean().fillna(0).loc[clusters[clusters == cl].index]
+            data.T.plot(legend = False, alpha = .2, color = 'black', ax = xs)
+            data.mean(axis = 0).plot(linewidth = 3., color = clustersColors[cl], ax = xs)
             n = len(data)
-            print('{}, N = {}'.format(names[cl], n))
-
+            print('{}, N = {}'.format(clustersNames[cl], n))
         ax1.spines['top'].set_visible(False)
         ax1.spines['right'].set_visible(False)
         ax2.spines['right'].set_visible(False)
         ax3.spines['right'].set_visible(False)
-
-        f.subplots_adjust(hspace=0)
-        plt.ylim(-0.02, 3.5)
-        plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
-
-        # plot time series starting from 2020-02-15
-        timeSeriesPlot = weeklyDF.loc['2020-02-15':].plot(legend = False, color = 'Black', linewidth = 0.2)
-        timeSeriesPlot.set(title = "COVID-19 Weekly Deaths Per One Million Population Worldwide")
-        plt.xlabel('', fontsize = 16)
-        plt.ylabel('Weekly Deaths', fontsize = 16)
+        f.subplots_adjust(hspace = 0)
+        plt.ylim(0, 40)
+        plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible = False)
         plt.show()
